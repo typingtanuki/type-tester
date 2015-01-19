e57091a1f31db6889f65228db8c0458c8e9359d9
Clerc Mathias <clerc@infoscience.co.jp>
2015-01-19 12:52:44 +0900

    Fix a trimArray

--- type-tester.js
+++ type-tester.js
@@ -1,13 +1,14 @@
 window.TypeTester = {
     trimArray: function (array) {
-		var out = [];
-		var val;
+        var out = [];
+        var val;
         for (var i = 0; i < array.length; i++) {
-             val = array[i].trim();
-             if(typeof val != "string" || val.length>0){
-			     out.push(val);
-			 }
+            val = array[i].trim();
+            if (typeof val != "string" || val.length > 0) {
+                out.push(val);
+            }
         }
+        return out;
     },
     isOptional: function (argument) {
         return argument.indexOf("[optional]") == 0;
@@ -34,15 +35,15 @@ window.TypeTester = {
     },
     check: function () {
         var fullFunction = arguments.callee.caller.toString();
-        var functionName = fullFunction.replace(/\r*\n*/,"").split("(")[0].split("function")[1].trim();
-        var parameterPart = fullFunction.replace(/\r*\n*/,"").split("(")[1].split(")")[0];
+        var functionName = fullFunction.replace(/\r*\n*/, "").split("(")[0].split("function")[1].trim();
+        var parameterPart = fullFunction.replace(/\r*\n*/, "").split("(")[1].split(")")[0];
         var parameters = parameterPart.split(",");
         parameters = this.trimArray(parameters);
 
         var parentArguments = arguments.callee.caller.arguments;
-        console.warn(parentArguments, parameters);
+
         if (parentArguments.length > parameters.length) {
-            console.warn("Too many arguments passed to the function '"+ functionName + "'.\nExpected " + parameters.length + " but got " +parentArguments.length)
+            console.warn("Too many arguments passed to the function '" + functionName + "'.\nExpected " + parameters.length + " but got " + parentArguments.length)
         }
 
         var i, parameter;
@@ -51,7 +52,7 @@ window.TypeTester = {
 
             if (parentArguments[i] == undefined || parentArguments[i] == null) {
                 if (!parameter.optional) {
-                    console.warn("Missing required parameter '" +parameter.name + "' in function '" + functionName + "' at index " + i+ " but got " + parentArguments[i]);
+                    console.warn("Missing required parameter '" + parameter.name + "' in function '" + functionName + "' at index " + i + " but got " + parentArguments[i]);
                 }
             } else {
                 var actual = typeof(parentArguments[i]);
@@ -74,7 +75,7 @@ window.TypeTester = {
                         isCorrect = parameter.type == actual;
                 }
                 if (!isCorrect) {
-                    console.warn("Wrong type given to parameter '" +parameter.name + "' in function '" + functionName + "'.\nExpected a '"+ parameter.type + "' but got type " + actual + ":" +JSON.stringify(parentArguments[i], null, 4));
+                    console.warn("Wrong type given to parameter '" + parameter.name + "' in function '" + functionName + "'.\nExpected a '" + parameter.type + "' but got type " + actual + ":" + JSON.stringify(parentArguments[i], null, 4));
                 }
             }
         }
@@ -82,7 +83,7 @@ window.TypeTester = {
         for (i = parentArguments.length; i < parameters.length; i++) {
             parameter = this.extractParameter(parameters[i]);
             if (!parameter.optional) {
-                console.warn("Missing required parameter '" +parameter.name + "' in function '" + functionName + "' at index " +i);
+                console.warn("Missing required parameter '" + parameter.name + "' in function '" + functionName + "' at index " + i);
             }
         }
     }
